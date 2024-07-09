01b20d415354f23f3cebcc414d35928496f7a8b0
RehanShaikh007 <rehanmukhtarshaikh7@gmail.com>
2024-07-09 17:09:10 +0530

    Files Added

---
+++ signup/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Signupp from './component/Signup/Signupp'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Signupp/>
+    </>
+  )
+}
+
+export default App
